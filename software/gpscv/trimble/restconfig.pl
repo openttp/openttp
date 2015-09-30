2e2822b64c0f6d039aefac58ff4078ceb0d74b8f
groundstate <michaeljwouters@gmail.com>
2015-09-30 12:06:04 +1000

    Proper support for the SMT360 and many cleanups

--- software/gpscv/trimble/restconfig.pl
+++ software/gpscv/trimble/restconfig.pl
@@ -3,7 +3,7 @@
 #
 # The MIT License (MIT)
 #
-# Copyright (c) 2015 Michael J. Wouters
+# Copyright (c) 2015  R. Bruce Warrington, Michael J. Wouters
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -23,23 +23,19 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-#
-# Reconfigures the Resolution XXX serial port
-# Tested for:
-# 	Resolution T
-#		Resolution SMTx
-#	
-#
+#restconfig - Perl script to configure the serial port 
+
 
 use Time::HiRes qw( gettimeofday);
 use TFLibrary;
 use POSIX;
 use Getopt::Std;
 use POSIX qw(strftime);
-use vars  qw($tmask $opt_r);
+use vars  qw($tmask $opt_d $opt_h $opt_v);
 
-$DEBUG=0;
-$VERSION="2.0";
+$RESOLUTION_T=0;
+#$RESOLUTION_SMT=1;
+$RESOLUTION_SMT_360=2;
 
 $0=~s#.*/##;
 
@@ -48,8 +44,8 @@ if (-d "$home/etc")  {$configpath="$home/etc";}  else
 	{$configpath="$home/Parameter_Files";} # backwards compatibility
 
 if (-d "$home/logs")  {$logpath="$home/logs";} else 
-	{$logpath="$home/Log_Files";} # backwards compatibility
-
+	{$logpath="$home/Log_Files";}
+	
 # More backwards compatibility fixups
 if (-e "$configpath/cggtts.conf"){
 	$configFile=$configpath."/cggtts.conf";
@@ -64,10 +60,26 @@ else{
 	exit;
 }
 
+if( !(getopts('dhv')) || ($#ARGV>=1)) {
+	select STDERR;
+ 	ShowHelp();
+	exit;
+}
+
+if ($opt_v){
+	print "$0 version $VERSION\n";
+	exit;
+}
+
+if ($opt_h){
+	ShowHelp();
+	exit;
+}
+
 # Check for an existing lock file
-$lockfile = $logpath."/rx.lock";
-if (-e $lockfile){
-	open(LCK,"<$lockfile");
+$lockFile = $logpath."/rx.lock";
+if (-e $lockFile){
+	open(LCK,"<$lockFile");
 	$pid = <LCK>;
 	chomp $pid;
 	if (-e "/proc/$pid"){
@@ -77,39 +89,84 @@ if (-e $lockfile){
 	close LCK;
 }
 
-&Initialise($configFile);
+open(LCK,">$lockFile");
+print LCK $$,"\n";
+close LCK;
 
-print "Configuring the serial port for 115200, no parity\n";
-print "Note! Default serial port settings for the Resolution XXX are assumed:\n";
-print " 9600 baud, odd parity, 8 data bits, 1 stop bit.\n";
+&Initialise(@ARGV==1? $ARGV[0] : $configFile);
+$Init{version}="2.0";
 
-# Open the serial port
+$rxModel=$RESOLUTION_SMT_360;
+if ($localMajorVersion == 2){
+	if ($Init{"receiver:model"} eq "Resolution T"){
+		$rxModel=$RESOLUTION_T;
+	}
+#	elsif ($Init{"receiver:model"} eq "Resolution SMT"){
+#		$rxmodel=$RESOLUTION_SMT;
+#	}
+	elsif ($Init{"receiver:model"} eq "Resolution SMT 360"){
+		$rxModel=$RESOLUTION_SMT_360;
+	}
+	else{
+		print "Unknown receiver model: ".$Init{"receiver:model"}."\n";
+		exit;
+	}
+}
+
+Debug("Receiver model = " .($rxModel==$RESOLUTION_T?"Resolution T":"Resolution SMT 360"));
+
+# Open the serial port to the receiver
 $rxmask="";
-if ($localMajorVersion == 1){
-	$port=$Init{"gps port"};
-}
-elsif($localMajorVersion == 2){
-	$port=$Init{"receiver:port"};
-}
-$port="/dev/$port" unless $port=~m#/#; # try to fix up the port name
+$port=&GetConfig($localMajorVersion,"gps port","receiver:port");
+$port="/dev/$port" unless $port=~m#/#;
+Debug("Opening $port");
 
 unless (`/usr/local/bin/lockport $port $0`==1) {
-	printf "! Could not obtain lock on $port. Exiting.\n";
+	print "! Could not obtain lock on $port. Exiting.\n";
 	exit;
 }
 
 $rx=&TFConnectSerial($port,
-		(ispeed=>B9600,ospeed=>B9600,iflag=>IGNBRK,
+		(ispeed=>0010002,ospeed=>0010002,iflag=>IGNBRK,
 			oflag=>0,lflag=>0,cflag=>CS8|CREAD|HUPCL|PARENB|PARODD|CLOCAL));
-			
 # Set up a mask which specifies this port for select polling later on
 vec($rxmask,fileno $rx,1)=1;
-  
+
+if ($rxModel==$RESOLUTION_T){
+	print "Note! Default serial port settings for the Resolution T are assumed:\n";
+	print " 9600 baud, odd parity, 8 data bits, 1 stop bit.\n";
+	$rx=&TFConnectSerial($port,
+		(ispeed=>B9600,ospeed=>B9600,iflag=>IGNBRK,
+			oflag=>0,lflag=>0,cflag=>CS8|CREAD|HUPCL|PARENB|PARODD|CLOCAL));
+}
+elsif ($rxModel==$RESOLUTION_SMT_360){
+		print "Note! Default serial port settings for the SMT 360 are assumed:\n";
+		print " 115200 baud, odd parity, 8 data bits, 1 stop bit.\n";
+		$rx=&TFConnectSerial($port,
+		(ispeed=>0010002,ospeed=>0010002,iflag=>IGNBRK,
+			oflag=>0,lflag=>0,cflag=>CS8|CREAD|HUPCL|PARENB|PARODD|CLOCAL));
+}
+
+
+#Initialise parameters
+
+print "Configuring the serial port for 115200, no parity .. wait a bit\n";
+
 sleep 3;
 
 print "Configuring ...\n";
 
 &SendCommand("\xBC\x00\x0b\x0b\x03\x00\x00\x00\x02\x02\x00");
+# port     
+# input baud rate 
+# output baud rate 
+# data bits
+# parity
+# stop bits
+# reserved
+# input protocol
+# output protocol
+# reserved
 
 sleep 3;
 
@@ -117,15 +174,12 @@ print "Closing the serial port ...\n";
 close $rx;
 sleep 3;
 
-# Reopen the serial port 
-print "Reopening the serial port at new speed\n";
+print "Reopening the serial port at new settings\n";
 
-$rxmask="";
 $rx=&TFConnectSerial($port,
       (ispeed=>0010002,ospeed=>0010002,iflag=>IGNBRK,
        oflag=>0,lflag=>0,cflag=>CS8|CREAD|HUPCL|CLOCAL));
 
-# Set up a mask which specifies this port for select polling later on
 vec($rxmask,fileno $rx,1)=1;
 
 print "Writing settings to FLASH\n";
@@ -135,46 +189,81 @@ sleep 3;
 
 print "Done! New port settings are 115200 baud, no parity\n";
 
-# Tidy up time
-
-`/usr/local/bin/lockport -r $port`;
-
 # end of main 
 
 #----------------------------------------------------------------------------
 sub Initialise 
 {
-  my $name=shift;
-  my ($err);
-  my @required=();
-  if ($localMajorVersion==1){
-		@required=( "gps port");
+	my $name=shift;
+	if ($localMajorVersion == 1){
+		my @required=("gps port");
 		%Init=&TFMakeHash($name,(tolower=>1));
 	}
-	elsif ($localMajorVersion==2){
-		@required=( "receiver:port");
+	elsif($localMajorVersion == 2){
+		@required=("receiver:port","receiver:status file");
 		%Init=&TFMakeHash2($name,(tolower=>1));
 	}
- 
+
+	if (!%Init){
+		print "Couldn't open $name\n";
+		exit;
+	}
+
+	my ($tag,$err);
+  
+	# Check that all required information is present
 	$err=0;
-	foreach (@required){
-		unless (defined $Init{$_}){
+	foreach (@required) {
+		unless (defined $Init{$_}) {
 			print STDERR "! No value for $_ given in $name\n";
 			$err=1;
 		}
 	}
 	exit if $err;
+
+}# Initialise
+
+#-----------------------------------------------------------------------------
+sub GetConfig()
+{
+	my($ver,$v1tag,$v2tag)=@_;
+	if ($ver == 1){
+		return $Init{$v1tag};
+	}
+	elsif ($ver==2){
+		return $Init{$v2tag};
+	}
 }
 
+#----------------------------------------------------------------------------
+sub FixPath()
+{
+	my $path=$_[0];
+	if (!($path=~/^\//)){
+		$path =$ENV{HOME}."/".$path;
+	}
+	return $path;
+}
+
+#----------------------------------------------------------------------------
+sub ShowHelp
+{
+	print "Usage: $0 [-h] [-d] [-v] [initfile]\n";
+	print "  -d debug\n";
+	print "  -h show this help\n";
+	print "  -v show version\n";
+	print "  The default initialisation file is $configFile\n";
+}
 
 #----------------------------------------------------------------------------
 sub Debug
 {
 	$now = strftime "%H:%M:%S",gmtime;
-	if ($DEBUG) {print "$now $_[0] \n";}
+	if ($opt_d) {print "$now $_[0] \n";}
 }
 
 #----------------------------------------------------------------------------
+
 sub SendCommand
 {
   my $cmd=shift;
