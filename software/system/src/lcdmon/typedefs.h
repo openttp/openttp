042750acbb089b63844af00c227e90fd6a29e3b8
groundstate <michaeljwouters@gmail.com>
2016-04-07 13:53:06 +1000

    Added license information

--- software/system/src/lcdmon/typedefs.h
+++ software/system/src/lcdmon/typedefs.h
@@ -1,9 +1,30 @@
-//============================================================================
-// 635 Linux Demonstration Code.
-// typedefs,h: Ultra-simple 635 command-line communications example
-// Copyright 2005, Crystalfontz America, Inc. Written by Brent A. Crosby
-// www.crystalfontz.com, brent@crystalfontz.com
-//============================================================================
+//
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2016  Michael J. Wouters
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+// Modification history
+//
+
 typedef unsigned char ubyte;
 typedef signed char sbyte;
 typedef unsigned short word;
@@ -15,4 +36,4 @@ typedef union
   word
     as_word;
   } WORD_UNION;
-//============================================================================
+
