8fe69cdd52a9b58003620764e12bd3cdeae232cd
groundstate <groundstate@users.noreply.github.com>
2016-08-19 08:11:24 +1000

    Check for missing RINEX, more command line options

--- software/gpscv/common/bin/runmktimetx.pl
+++ software/gpscv/common/bin/runmktimetx.pl
@@ -24,16 +24,17 @@
 # THE SOFTWARE.
 
 # 2016-05-02 MJW First version. A minimal script
-#
+# 2016-08-18 MJW Generate missing RINEX
 
 use POSIX;
 use Getopt::Std;
 use TFLibrary;
 
-use vars qw($opt_d $opt_h $opt_v $opt_x);
+use vars qw($opt_a $opt_d $opt_h $opt_v $opt_x);
 
-$VERSION="0.1.0";
+$VERSION="0.1.1";
 $AUTHORS="Michael Wouters";
+$MAX_AGE=7; # number of days to look backwards for missing files
 
 # Check command line
 if ($0=~m#^(.*/)#) {$path=$1} else {$path="./"}	# read path info
@@ -59,7 +60,7 @@ if (!(-e $configFile)){
 	ErrorExit("The configuration file $configFile was not found!\n");
 }
 
-if (!(getopts('dhvx')) || $opt_h){
+if (!(getopts('a:dhvx')) || $opt_h){
 	&ShowHelp();
 	exit;
 }
@@ -70,14 +71,26 @@ if ($opt_v){
 	exit;
 }
 
+$opt_a = $MAX_AGE;
+if ($opt_a){
+	$maxAge=$opt_a;
+}
+
 $mjdPrev =  int(time()/86400 + 40587-1); # yesterday
 $mjdStart = $mjdStop= $mjdPrev;
 
-if ($#ARGV == 1){
+if ($#ARGV == -1){
+	# process previous day
+}
+elsif ($#ARGV == 0){
+	$mjdStart = $ARGV[0];
+	$mjdStop = $mjdStart;
+}
+elsif ($#ARGV == 1){
 	$mjdStart = $ARGV[0];
 	$mjdStop  = $ARGV[1];
 }
-elsif ($#ARGV != -1){
+else{
 	ShowHelp();
 	exit;
 }
@@ -107,27 +120,32 @@ else{
 	}
 	$ctrDataPath = TFMakeAbsolutePath($Init{"paths:counter data"},$home);
 
-	
 	if (!($Init{"receiver:file extension"} =~ /^\./)){ # do we need a period ?
 		$rxExt = ".".$Init{"receiver:file extension"};
 	}
 	$rxDataPath = TFMakeAbsolutePath($Init{"paths:receiver data"},$home);
 	
+	$createRINEX=lc($Init{"rinex:create"}) eq 'yes';
+	$rnxDataPath="";
+	if ($createRINEX){
+		$rnxDataPath= TFMakeAbsolutePath($Init{"paths:rinex"},$home);
+	}
+	
 	if (lc($Init{"cggtts:create"}) eq 'yes'){
 		# Just search within the last 7 days for missed processing
 
-		for ($m=$mjdPrev-7;$m<=$mjdPrev;$m++){
-			Debug("Checking $m");
+		for ($mjd=$mjdPrev-$maxAge;$mjd<=$mjdPrev;$mjd++){
+			Debug("Checking $mjd");
 			
 			# If there is a missing raw data file, then just skip
-			$f = $ctrDataPath.$m.$ctrExt;
+			$f = $ctrDataPath.$mjd.$ctrExt;
 			if (!(-e $f || -e "$f.gz")){
-				Debug("$f is missing");
+				Debug("$f is missing - skipping");
 				next;
 			}
-			$f = $rxDataPath.$m.$rxExt;
+			$f = $rxDataPath.$mjd.$rxExt;
 			if (!(-e $f || -e "$f.gz")){
-				Debug("$f is missing");
+				Debug("$f is missing - skipping");
 				next;
 			}
 			
@@ -142,7 +160,7 @@ else{
 				Debug("\tchecking CGGTTS output $output");
 				$cggttsPath = TFMakeAbsolutePath($Init{"$output:path"},$home);
 				if (lc($Init{"cggtts:naming convention"}) eq "plain"){
-					$cggttsFile = $cggttsPath.$m.".cctf";
+					$cggttsFile = $cggttsPath.$mjd.".cctf";
 				}
 				elsif (lc($Init{"cggtts:naming convention"}) eq "bipm"){
 					
@@ -153,21 +171,42 @@ else{
 					elsif ($c eq 'glonass'){
 						$constellation='R';
 					}
-					$ext = sprintf "%2d.%03d",int ($m/1000),$m - (int $m/1000)*1000;
+					$ext = sprintf "%2d.%03d",int ($mjd/1000),$mjd - (int $mjd/1000)*1000;
 					$cggttsFile = $cggttsPath.$constellation."M".$Init{"cggtts:lab id"}.$Init{"cggtts:receiver id"}.$ext;
 				}
 				Debug("\tchecking $cggttsFile");
 				if (!(-e $cggttsFile)){ # only attempt to regenerate missing files 
 					Debug("\t-->missing");
-					Debug("\t-->processing $m");
-					`$binPath/mktimetx -m $m`;
+					Debug("\t-->processing $mjd");
+					`$binPath/mktimetx -m $mjd`;
 				}
 				else{
 					Debug("\t-->exists");
 				}
 			}
 			
-			# TODO look for missing RINEX files
+			# Look for missing RINEX files
+			# TODO this will regenerate CGGTTS files
+			next unless ($createRINEX);
+			
+			# Convert MJD to YY and DOY
+			$t=($mjd-40587)*86400.0;
+			@tod = gmtime($t);
+			$yy = $tod[5]-(int($tod[5]/100))*100;
+			$doy = $tod[7]+1;
+	
+			$rnxFile = sprintf "%s%s%03d0.%02dO", $rnxDataPath,$Init{"antenna:marker name"},$doy,$yy;
+			$rnxFilegzip = $rnxFile.'.gz';
+			Debug("\tchecking $rnxFile(.gz)");
+			if (!(-e $rnxFile || -e $rnxFilegzip)){
+				Debug("\t-->missing");
+				Debug("\t-->processing $mjd");
+				`$binPath/mktimetx -m $mjd`;
+			}
+			else{
+				Debug("\t-->exists");
+			}
+			
 		}
 	} 
 }
@@ -179,6 +218,8 @@ else{
 sub ShowHelp
 {
 	print "Usage: $0 [OPTION] ... [startMJD] [stopMJD]\n";
+	print "\t-a <num> maximum age of files to look for when reprocessing\n";
+	print "            missing files, in days (default $MAX_AGE)\n";
 	print "\t-d debug\n";
   print "\t-h show this help\n";
 	print "\t-x run missed processing\n";
