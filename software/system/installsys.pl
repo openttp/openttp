909065fbf13bf736f5f7ed4b0960bd4341d71f56
groundstate <michaeljwouters@gmail.com>
2016-11-25 13:19:10 +1100

    More functionality added to the installation script.

--- software/system/installsys.pl
+++ software/system/installsys.pl
@@ -36,15 +36,38 @@ use Carp;
 use FileHandle;
 use POSIX;
 
-use vars qw($opt_h $opt_m $opt_t $opt_v);
+use vars qw($opt_h $opt_i $opt_l $opt_m $opt_t $opt_v);
 
 $0=~s#.*/##;	# strip path
 
 $VERSION = "version 0.1";
 $ECHO=1;
 
-if (!getopts('hmtv') || $opt_h){
+
+@defaulttargets = ("libconfigurator","dioctrl","lcdmon","ppsd","sysmonitor","tflibrary","kickstart","misc");
+
+if (grep (/^tflibrary/,@targets)){
+	# Find a place in /usr/local to put it
+	foreach $dir (@INC){
+		if ($dir =~ /\/usr\/local\//){
+			if (-e $dir){
+				Log("Installed TFLibrary to $dir\n",$ECHO);
+			}
+		}
+	} 
+}
+
+if (!getopts('hi:lmtv') || $opt_h){
 	ShowHelp();	
+	exit;
+}
+
+@targets=@defaulttargets;
+if ($opt_l){
+	print "Available targets:\n";
+	foreach $target (@targets){
+		print "$target\n";
+	}
 	exit;
 }
 
@@ -53,6 +76,10 @@ if ($opt_v){
 	exit;
 }
 
+if ($opt_i){
+	@targets = ($opt_i);
+}
+	
 if ($EFFECTIVE_USER_ID >0){
 	print "$0 must be run as superuser!\n";
 	exit;
@@ -60,12 +87,43 @@ if ($EFFECTIVE_USER_ID >0){
 
 open (LOG,">./installsys.log");
 
-CompileTarget('libconfigurator','src/libconfigurator','install');
-CompileTarget('dioctrl','src/dioctrl','install');
-CompileTarget('lcdmon','src/lcdmon','install');
-#CompileTarget('okbitloader','src/okbitloader','install');
-#CompileTarget('okcounterd','src/okcounterd','install');
-CompileTarget('ppsd','src/ppsd','install');
+if (grep (/^libconfigurator/,@targets)) {CompileTarget('libconfigurator','src/libconfigurator','install')};
+if (grep (/^dioctrl/,@targets)) {CompileTarget('dioctrl','src/dioctrl','install');}
+if (grep (/^lcdmon/,@targets)) {CompileTarget('lcdmon','src/lcdmon','install');}
+#if (grep (/^okbitloader/,@targets)) {CompileTarget('okbitloader','src/okbitloader','install');}
+#if (grep (/^okcounterd/,@targets)) {CompileTarget('okcounterd','src/okcounterd','install');}
+if (grep (/^ppsd/,@targets)) {CompileTarget('ppsd','src/ppsd','install');}
+if (grep (/^misc/,@targets)) {CompileTarget('misc','src','install');}
+
+if (grep (/^kickstart/,@targets)){
+	`cp src/kickstart.pl /usr/local/bin`;
+	Log("Installed kickstart.pl to /usr/local/bin\n",$ECHO);
+}
+
+# Installation of sysmonitor
+if (grep (/^sysmonitor/,@targets)){
+	MakeDirectory('/usr/local/log');
+	MakeDirectory('/usr/local/log/alarms');
+	InstallScript('src/sysmonitor.pl','/usr/local/bin','src/sysmonitor.conf','/usr/local/etc');
+}
+
+# Installation of TFLibrary
+$installed=0;
+if (grep (/^tflibrary/,@targets)){
+	# Find a place in /usr/local to put it
+	foreach $dir (@INC){
+		if ($dir =~ /\/usr\/local\//){
+			if (-e $dir){
+				`cp src/TFLibrary.pm $dir`;
+				Log("Installed TFLibrary to $dir\n",$ECHO);
+				$installed = 1;
+			}
+		}
+	} 
+	if (!$installed){Log("Failed to install TFLibrary\n",$ECHO);}
+}
+
+
 close LOG;
 
 # ------------------------------------------------------------------------
@@ -73,6 +131,8 @@ sub ShowHelp
 {
 	print "$0 $VERSION\n";
 	print "\t-h print help\n";
+	print "\t-i <target> install target\n"; 
+	print "\t-l list targets\n";
 	print "\t-m minimal installation\n";
 	print "\t-t verbose\n";
 	print "\t-v print version\n";
@@ -106,8 +166,34 @@ sub CompileTarget
 	my $makeout = `make clean && make $makeargs 2>&1`;
 	if ($opt_t) {print $makeout;}
 	if ($? >> 8){
-		Log ("\nCompilation/installation of $target failed :-(\n",$ECHO);
+		Log ("\nCompilation/installation of $target failed :-(\n",$ECHO);# ------------------------------------------------------------------------
 		exit(1);
 	}
 	chdir $cwd;
+}
+
+# ------------------------------------------------------------------------
+sub MakeDirectory
+{
+	$dir = $_[0];
+	if (!(-e $dir)){
+		`mkdir $dir`;
+		Log("Created $dir\n",$ECHO);
+	}
+	else{
+		Log("$dir already exists - skipped\n",$ECHO);
+	}
+}
+
+# ------------------------------------------------------------------------
+# Arguments are pairs of (file,destination)
+sub InstallScript
+{
+	my @targets = @_;
+	my $i;
+	for ($i=0;$i<=$#targets;$i+=2){
+		`cp $targets[$i] $targets[$i+1]`;
+		Log("Copied $targets[$i] to $targets[$i+1]\n",$ECHO);
+	}
+	
 }
