2a312ccbd5fcd805d4a49b45661fc3b2b5b54457
groundstate <michaeljwouters@gmail.com>
2018-05-15 09:04:05 +1000

    Extract more than one observation from the RINEX file

--- software/gpscv/common/bin/xrnxobs.pl
+++ software/gpscv/common/bin/xrnxobs.pl
@@ -29,15 +29,14 @@
 # 2015-08-20 MJW First version
 # 2016-08-09 MJW Handle more than 12 observations properly. Handle blank entries in RINEX observation records
 # 2017-02-07 MJW Bug fixes for multiline observations with missing observations.
-#
+# 2018-05-15 MJW Extract more than one observation
 
 use POSIX qw(floor ceil);
 
-$obs=$ARGV[0];
 $fin=$ARGV[1];
 
 if ($#ARGV!=1){
-	print "Usage: xrnxobs.pl observation_name rinex_obs_file\n";
+	print "Usage: xrnxobs.pl observation_name[,observation_name,...] rinex_obs_file\n";
 	exit;
 }
 
@@ -54,36 +53,48 @@ if ($verinfo[1] =~ /2\.\d+/){
 }
 elsif ($verinfo[1] =~ /3\.\d+/){
 	$rnxver=3;
-	print "Sorry,RINEX V3 is not supported\n";
+	print "Sorry,RINEX V3 is not supported yet.\n";
 	exit;
 }
 
 if ($rnxver == 0){
-	print "Can't determine the RINEX version\n";
+	print "Can't determine the RINEX version.\n";
 	exit;
 }
 
+@obs = split /,/,$ARGV[0];
+
 while ($line=<IN>){
 	if ($line=~/TYPES OF OBSERV/){
-		if (!($line =~ /$obs/)){
-			print "The observation $obs is not present\n";
-			exit;
+		for ($i=0;$i<=$#obs;$i++){
+			if (!($line =~ /$obs[$i]/)){
+				print "The observation $obs[$i] is not present\n";
+				exit;
+			}
 		}
 		@obstypes= split " ",( substr $line,0,60);
 		$nobs=$obstypes[0];
-		for ($i=1;$i<=$nobs;$i++){
-			if ($obstypes[$i] eq $obs){
-				$obscol=$i-1;
-				last;
+		for ($i=0;$i<=$#obs;$i++){
+			for ($n=1;$n<=$nobs;$n++){
+				if ($obstypes[$n] eq $obs[$i]){
+					$obscol[$i]=$n-1;
+					last;
+				}
 			}
 		}
-		printf "     1    %2s                                                # / TYPES OF OBSERV \n",$obs;
+		printf("%6d",$#obs + 1);
+		for ($i=0;$i<=$#obs;$i++){
+			printf("%4s%2s"," ",$obs[$i]);
+		}
+		printf(" " x (60 - ($#obs+1+1)*6));
+		printf("# / TYPES OF OBSERV \n");
 	}
 	else{
 		print $line;
 	}
 	last if ($line =~/END OF HEADER/);
 }
+
 
 while ($line=<IN>){
 	if ($rnxver == 2){
@@ -122,20 +133,22 @@ while ($line=<IN>){
 			# Each field should be 16 characters long
 			# Round up the length to the next multiple of 16
 			$nfields = ceil($len/16);
-			for ($f=0;$f<$nfields;$f++){
-				$val = substr $line, $f*16, (($f+1)*16<=$len?16:$len-$f*16);
-				if ($f == $obscol){
-					# Is it a number ?
-					if ($val =~/\d+\.\d+/){
-						printf "%16s\n",$val;
+			for ($o=0;$o<=$#obs;$o++){
+				for ($f=0;$f<$nfields;$f++){
+					$val = substr $line, $f*16, (($f+1)*16<=$len?16:$len-$f*16);
+					if ($f == $obscol[$o]){
+						# Is it a number ?
+						if ($val =~/\d+\.\d+/){
+							printf "%16s",$val;
+						}
+						else{ # nahh
+							printf "%16s"," "; # blank it
+						}
+						last;
 					}
-					else{ # nahh
-						printf "%16s\n","0.00000";
-					}
-					last;
 				}
 			}
-			
+			printf("\n");
 		}
 	}
 	elsif ($rnxver == 3){
