80b2874a4331e66852267e44782e00f09cc2d4cb
groundstate <groundstate@users.noreply.github.com>
2018-11-18 19:44:51 +1100

    Semi-functional

--- software/gpscv/common/bin/diffcggtts.py
+++ software/gpscv/common/bin/diffcggtts.py
@@ -25,7 +25,11 @@
 # THE SOFTWARE.
 #
 # Differences pairs of CGGTTS files
-# Doesn't do much at present
+# 
+# Example
+# diffcggtts.py ref cal 58418 58419
+# 
+# diffcggtts.py --debug --refext gps.C1.30s.dat --calext gps.C1.30s.dat  ./ ../ottp1 58418 58419
 #
 
 import argparse
@@ -55,7 +59,7 @@ def Warn(msg):
 	return
 
 # ------------------------------------------
-def ReadCGGTTS(fname):
+def ReadCGGTTS(fname,mjd):
 	d=[]
 	Debug('Reading ' + fname)
 	try:
@@ -82,18 +86,26 @@ def ReadCGGTTS(fname):
 		match = re.match('^\s+hhmmss',l)
 		if match:
 			break
-		
+	nextday =0
 	while True:
 		l = fin.readline().rstrip()
 		if l:
 			fields = l.split()
+			theMJD = int(fields[1])
 			hh = int(fields[2][0:2])
 			mm = int(fields[2][2:4])
 			ss = int(fields[2][4:6])
-			d.append([fields[0],fields[1],hh*3600+mm*60+ss,fields[3],fields[4],fields[5],fields[6],fields[7],fields[8],fields[9],fields[10],fields[11]])
+			tt = hh*3600+mm*60+ss
+			if ((tt < 86399 and theMJD == mjd) or args.keepall):
+				d.append([fields[0],int(fields[1]),tt,int(fields[3]),int(fields[4]),int(fields[5]),int(fields[6]),int(fields[7]),int(fields[8]),int(fields[9]),int(fields[10]),fields[11]])
+			else:
+				nextday += 1
 		else:
 			break
+		
 	fin.close()
+	
+	Debug(str(nextday) + ' tracks after end of the day removed')
 	
 	return d
 	
@@ -113,6 +125,7 @@ parser.add_argument('--debug','-d',help='debug (to stderr)',action='store_true')
 parser.add_argument('--nowarn',help='suppress warnings',action='store_true')
 parser.add_argument('--refext',help='file extension for reference receiver (default = cctf)')
 parser.add_argument('--calext',help='file extension for calibration receiver (default = cctf)')
+parser.add_argument('--keepall',help='keep all tracks ,including those after the end of the GPS day',action='store_true')
 parser.add_argument('--version','-v',help='show version and exit',action='store_true')
 
 args = parser.parse_args()
@@ -139,10 +152,87 @@ allref=[]
 allcal=[]
 for mjd in range(firstMJD,lastMJD+1):
 	fref = args.refDir + '/' + str(mjd) + '.' + refExt
-	d=ReadCGGTTS(fref)
+	d=ReadCGGTTS(fref,mjd)
 	allref = allref + d
 
+	fref = args.calDir + '/' + str(mjd) + '.' + calExt
+	d=ReadCGGTTS(fref,mjd)
+	allcal = allcal + d
+	
 # Now match tracks
 
+matches = []
+reflen = len(allref)
+callen = len(allcal)
+iref=0
+jcal=0
 
+PRN=0
+MJD=1
+STTIME=2
+REFSYS=6
+
+#print reflen,callen
+
+while iref < reflen:
+	
+	mjd1=allref[iref][MJD]
+	prn1=allref[iref][PRN]
+	st1 =allref[iref][STTIME]
+	
+	while (jcal < callen):
+		mjd2=allcal[jcal][MJD]
+		st2 =allcal[jcal][STTIME]
+		
+		if (mjd2 > mjd1):
+			break # stop searching - move pointer 1
+		elif (mjd2 < mjd1):
+			jcal += 1 # need to move pointer 2 forward
+		elif (st2 > st1):
+			break # stop searching - need to move pointer2
+		elif ((mjd1==mjd2)and (st1 == st2)):
+			# times are matched so search for SV
+			while ((mjd1 == mjd2) and (st1 == st2) and (jcal<callen)):
+	
+				mjd2=allcal[jcal][MJD]
+				st2 =allcal[jcal][STTIME]
+				prn2=allcal[jcal][PRN]
+				if (prn1 == prn2):
+					break
+				jcal += 1
+			if ((mjd1 == mjd2) and (st1 == st2) and (prn1 == prn2)):
+				# match!
+				print mjd1,st1,prn1,allref[iref][REFSYS],allcal[jcal][REFSYS]
+				matches.append(allref[iref]+allcal[jcal])
+				jcal += 1
+				break
+			else:
+				break
+		else:
+			jcal += 1
+			
+	iref +=1
+	
+#print len(matches)
+#print matches
+avmatches=[]
+
+lenmatch=len(matches)
+mjd1=matches[1][MJD]
+st1=matches[1][STTIME]
+av = 0
+nsv = 1
+imatch=2
+while imatch < lenmatch:
+	mjd2 = matches[imatch][MJD]
+	st2 = matches[imatch][STTIME]
+	if (mjd1==mjd2 and st1==st2):
+		nsv +=1
+	else:
+		print nsv
+		mjd1 = mjd2
+		st1  = st2
+		nsv=1
+	imatch += 1
+	
 	
