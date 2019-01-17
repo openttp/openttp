f10c6e28a44001923c4e3620a30d810744e818b0
groundstate <michaeljwouters@gmail.com>
2019-01-17 00:48:33 +0000

    Limited supported for V3 mixed files

--- software/gpscv/common/bin/runr2cggttsv8.py
+++ software/gpscv/common/bin/runr2cggttsv8.py
@@ -35,7 +35,7 @@ import subprocess
 import sys
 import time
 
-VERSION = "0.1"
+VERSION = "0.1.1"
 AUTHORS = "Michael Wouters"
 
 # ------------------------------------------
@@ -58,32 +58,65 @@ def MJD(unixtime):
 
 # ------------------------------------------
 #
-def MakeRINEXObservationName(dirname,staname,yy,doy,reqd):
-	fname = '{}/{}{:03d}0.{:02d}o'.format(dirname,staname,doy,yy)
-	if (os.path.exists(fname)):
-		return fname
-	else:
+def MakeRINEXObservationName(dirname,staname,yyyy,doy,rnxver,reqd):
+	if (rnxver == 2):
+		yy = yyyy - int(yyyy/100)*100
+		fname = '{}/{}{:03d}0.{:02d}o'.format(dirname,staname,doy,yy)
+		Debug('Trying '+fname)
+		if (os.path.exists(fname)):
+			return fname
+		
 		fname = '{}/{}{:03d}0.{:02d}O'.format(dirname,staname,doy,yy)
 		if (os.path.exists(fname)):
 			return fname
-	if (reqd):	
-		print "Can't open",fname,"(or .o)"
-		exit()
+		
+		if (reqd):	
+			print "Can't open",fname,"(or .o)"
+			exit()
+	elif (rnxver==3):
+		# Try a V3 name first
+		fname = '{}/{}_R_{:04d}{:03d}0000_01D_30S_MO.crx'.format(dirname,staname,yyyy,doy)
+		Debug('Trying '+fname)
+		
+		yy = yyyy - int(yyyy/100)*100
+		fname = '{}/{}{:03d}0.{:02d}o'.format(dirname,staname,doy,yy)
+		Debug('Trying '+fname)
+		if (os.path.exists(fname)):
+			return fname
+		
+		fname = '{}/{}{:03d}0.{:02d}O'.format(dirname,staname,doy,yy)
+		if (os.path.exists(fname)):
+			return fname
+		
+		if (reqd):	
+			print "Can't open",fname,"(or .o)"
+			exit()
 	return ''
 
 # ------------------------------------------		
 #
-def MakeRINEXNavigationName(dirname,staname,yy,doy,reqd):
-	fname = '{}/{}{:03d}0.{:02d}n'.format(dirname,staname,doy,yy)
-	if (os.path.exists(fname)):
-		return fname
-	else:
+def MakeRINEXNavigationName(dirname,staname,yyyy,doy,rnxver,reqd):
+	if (rnxver == 2):
+		yy = yyyy - int(yyyy/100)*100
+		fname = '{}/{}{:03d}0.{:02d}n'.format(dirname,staname,doy,yy)
+		if (os.path.exists(fname)):
+			return fname
+		
 		fname = '{}/{}{:03d}0.{:02d}N'.format(dirname,staname,doy,yy)
 		if (os.path.exists(fname)):
 			return fname
-	if (reqd):
-		print "Can't open",fname,"(or .N)"
-		exit()
+		
+		if (reqd):
+			print "Can't open",fname,"(or .N)"
+			exit()
+	elif (rnxver == 3):
+		# Mixed navigation files only
+		fname = '{}/{}_R_{:04d}{:03d}0000_01D_MN.rnx'.format(dirname,staname,yyyy,doy)
+		if (os.path.exists(fname)):
+			return fname
+		if (reqd):
+			print "Can't open",fname
+			exit()
 	return ''
 
 # ------------------------------------------
@@ -101,6 +134,10 @@ parser.add_argument('cggttsout',help='CGGTTS output path',type=str)
 parser.add_argument('--debug','-d',help='debug (to stderr)',action='store_true')
 parser.add_argument('--rename','-r',help='rename cggtts output',action='store_true')
 parser.add_argument('--version','-v',help='show version and exit',action='store_true')
+parser.add_argument('--rinexversion',help='show version and exit')
+
+
+rnxVersion = 2
 
 args = parser.parse_args()
 
@@ -130,21 +167,26 @@ PARS=args.params
 # Convert MJD to YY and DOY
 t=(mjd-40587)*86400.0
 tod=time.gmtime(t)
-yy1 = tod.tm_year - int(tod.tm_year/100)*100
+yyyy1 =  tod.tm_year
+#yy1 = tod.tm_year - int(tod.tm_year/100)*100
 doy1 = tod.tm_yday
 
 # and for the following day
 t=(mjd+1-40587)*86400.0;
 tod=time.gmtime(t)
-yy2 = tod.tm_year - int(tod.tm_year/100)*100
+yyyy2 =  tod.tm_year
+#yy2 = tod.tm_year - int(tod.tm_year/100)*100
 doy2 = tod.tm_yday
+
+if args.rinexversion:
+	rnxVersion = int(args.rinexversion)
 
 
 # Make RINEX file names
-rnx1=MakeRINEXObservationName(RNXOBSDIR,STA,yy1,doy1,True);
-nav1=MakeRINEXNavigationName(RNXNAVDIR,NAV,yy1,doy1,True);
-rnx2=MakeRINEXObservationName(RNXOBSDIR,STA,yy2,doy2,False); #don't require next day
-nav2=MakeRINEXNavigationName(RNXNAVDIR,NAV,yy2,doy2,False);
+rnx1=MakeRINEXObservationName(RNXOBSDIR,STA,yyyy1,doy1,rnxVersion,True);
+nav1=MakeRINEXNavigationName(RNXNAVDIR,NAV,yyyy1,doy1,rnxVersion,True);
+rnx2=MakeRINEXObservationName(RNXOBSDIR,STA,yyyy2,doy2,rnxVersion,False); #don't require next day
+nav2=MakeRINEXNavigationName(RNXNAVDIR,NAV,yyyy2,doy2,rnxVersion,False);
 
 if (os.path.exists(PARS)):
 	if (PARS != 'paramCGGTTS.dat'):
@@ -191,11 +233,18 @@ shutil.copyfile(PARS+'.bak',PARS)
   
 # Almost there ...
 shutil.copy(rnx1,'rinex_obs')
-shutil.copy(nav1,'rinex_nav_gps')
+if (rnxVersion == 2):
+	shutil.copy(nav1,'rinex_nav_gps')
+elif (rnxVersion == 3):
+	shutil.copy(nav1,'rinex_nav_mix')
+	
 if (rnx2 != ''):
 	shutil.copy(rnx2,'rinex_obs_p')
 if (nav2 != ''):
-	shutil.copy(nav2,'rinex_nav_p_gps')
+	if (rnxVersion == 2):
+		shutil.copy(nav1,'rinex_nav_p_gps')
+	elif (rnxVersion == 3):
+		shutil.copy(nav1,'rinex_nav_p_mix')
 
 subprocess.call(['r2cggttsv8'])
 
