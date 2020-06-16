136cfd2ae19d10bc10fa946ffd32b838b112f707
ELouisMarais <louis.marais@measurement.gov.au>
2020-06-16 01:14:32 +0000

    Added checks for log1wtemp and logpicputemp

--- software/gpscv/common/bin/checkstatus.pl
+++ software/gpscv/common/bin/checkstatus.pl
@@ -43,8 +43,9 @@
 # 30-03-2016 MJW Rewrite of check_status. Uses gpscv.conf to determine a few paths etc.
 #								 Closer conformance with our 'standard' Perl script.
 # 2018-09-13 ELM Added ubloxlog to executables to be checked
-# 2020-06-16 ELM Added hp5313xlog, LTElog, nv08log and ticclog to executable to be
-#                checked and changed -107 > -7 in approx. line 123.
+# 2020-06-16 ELM Added hp5313xlog, LTElog, nv08log, ticclog, log1Wtemp and 
+#                logpicputemp to executable to be checked.
+#                Also changed -107 > -7 in approx. line 123; we only want last 7 days.
 #
 
 use Sys::Hostname;
@@ -124,7 +125,7 @@ if (defined $Init{"paths:counter data"}){
  "find $cggttspath -mtime -7 -printf \"%Ab %Ad %AH:%AM %s\t%f\n\" | sort ",
 # 'ps x | grep --extended-regexp "jnslog|restlog|prs10log|okxemlog|plrxlog" | grep -v grep',
 # 'ps x | grep --extended-regexp "jnslog|restlog|prs10log|okxemlog|plrxlog|ubloxlog" | grep -v grep',
- 'ps x | grep --extended-regexp "jnslog|restlog|prs10log|okxemlog|plrxlog|ubloxlog|hp5313xlog|LTElog|nv08log|ticclog" | grep -v grep',
+ 'ps x | grep --extended-regexp "jnslog|restlog|prs10log|okxemlog|plrxlog|ubloxlog|hp5313xlog|LTElog|nv08log|ticclog|log1Wtemp|logpicputemp" | grep -v grep',
  'find . -name "*.check" -printf "%Ab %Ad %AH:%AM %f\n"',
  'ps ax | grep ntpd | grep -v grep & /usr/local/bin/ntpq -p'
  );
