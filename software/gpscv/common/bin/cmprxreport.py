c17a4eb9cf560472235d6065bbd1a3987c83e2c4
groundstate <michaeljwouters@gmail.com>
2023-12-12 04:03:37 +0000

    Functional

--- software/gpscv/common/bin/cmprxreport.py
+++ software/gpscv/common/bin/cmprxreport.py
@@ -4,7 +4,7 @@
 #
 # The MIT License (MIT)
 #
-# Copyright (c) 2018 Michael J. Wouters
+# Copyright (c) 2023 Michael J. Wouters
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -33,9 +33,15 @@ from   datetime import datetime
 import numpy as np
 import os
 import re
+import socket
 import subprocess
 import sys
 import time
+
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
 
 import allantools
 
@@ -49,7 +55,7 @@ try:
 except ImportError:
 	sys.exit('ERROR: Must install ottplib\n eg openttp/software/system/installsys.py -i ottplib')
 
-VERSION = "0.0.2"
+VERSION = "0.1.0"
 AUTHORS = "Michael Wouters"
 
 MODE_CMPALL = 0
@@ -155,11 +161,16 @@ def PlotEventMarkers(ax,rxev,evcol):
 			ax.add_patch(mplt.patches.Rectangle([tStart,ymin],tStop-tStart,ymax - ymin,facecolor=rBad))
 # ------------------------------------------
 
-
-home = os.environ['HOME'] 
+appName = os.path.basename(sys.argv[0])
+home = os.environ['HOME']
+user =os.environ['USER']
 root = home 
 configFile = os.path.join(root,'etc','cmprxreport.conf')
 tmpDir = './' 
+repDir = tmpDir
+emailRecipients = ''
+emailSender = ''
+smtpServer = ''
 
 cggttsTool = '/usr/local/bin/cmpcggtts.py'
 mode  = MODE_CMPALL
@@ -178,9 +189,16 @@ group = parser.add_mutually_exclusive_group()
 group.add_argument('--cmpall',help='compute all comparisons (default)',action='store_true')
 group.add_argument('--cmpref',help='compute only comparisons with reference receiver',action='store_true')
 
+group = parser.add_mutually_exclusive_group()
+group.add_argument('--monthly',help='report for past month',action='store_true')
+group.add_argument('--quarterly',help='report for past quarter',action='store_true')
+group.add_argument('--yearly',help='report for past year',action='store_true')
+
 parser.add_argument('--config','-c',help='use an alternate configuration file',default=configFile)
 
 parser.add_argument('--display',help='display plots',action='store_true')
+parser.add_argument('--report',help='create a report',action='store_true')
+parser.add_argument('--email',help='email report',action='store_true')
 parser.add_argument('--debug','-d',help='debug (to stderr)',action='store_true')
 parser.add_argument('--nowarn',help='suppress warnings',action='store_true')
 parser.add_argument('--quiet',help='suppress all output to the terminal',action='store_true')
@@ -213,6 +231,18 @@ if 'paths:root' in cfg:
 
 if 'paths:tmp' in cfg:
 	tmpDir = ottp.MakeAbsolutePath(cfg['paths:tmp'],root)
+
+if 'paths:reports' in cfg:
+	repDir = ottp.MakeAbsolutePath(cfg['paths:reports'],root)
+
+if 'main:email sender' in cfg:
+	emailSender = cfg['main:email sender']
+
+if 'main:email recipients' in cfg:
+	emailRecipients = cfg['main:email recipients']
+
+if 'main:smtp server' in cfg:
+	smtpServer = cfg['main:smtp server']
 	
 startMJD = ottp.MJD(time.time()) - 1 # previous day
 stopMJD  = startMJD
@@ -228,7 +258,23 @@ if (args.mjd):
 			ottp.ErrorExit('Stop MJD is before start MJD')
 	else:
 		ottp.ErrorExit('Too many MJDs')
-		
+
+repInterval = ''
+if (args.monthly):
+	stopMJD = ottp.MJD(time.time()) - 1
+	startMJD = stopMJD - 30
+	repInterval = 'last month'
+	
+if (args.quarterly):
+	stopMJD = ottp.MJD(time.time()) - 1
+	startMJD = stopMJD - 90
+	repInterval = 'last quarter'
+	
+if (args.yearly):
+	stopMJD = ottp.MJD(time.time()) - 1
+	startMJD = stopMJD - 365
+	repInterval = 'last year'
+	
 if args.ndays: # overrides args.mjd
 	currMJD = ottp.MJD(time.time()) - 1
 	startMJD = currMJD - 1 - int(args.ndays) - 1
@@ -264,6 +310,9 @@ cmd.append(tmpDir)
 
 rx1evcol = 'tab:green'
 rx2evcol = 'tab:purple'
+
+tmpFiles = []
+plotFiles = []
 
 for i in range(0,len(rx1list)):
 	rx1 = rx1list[i]
@@ -320,6 +369,7 @@ for i in range(0,len(rx1list)):
 				sufficientData = True
 				if os.path.isfile(matchFile):
 					dataFile  = os.path.join(tmpDir,'{}.{}.{}.av.matches.txt'.format(rx1,rx2,c))
+					tmpFiles.append(dataFile)
 					os.rename(matchFile,dataFile)
 					td = LoadRefCalAvMatches(dataFile)
 					sufficientData = len(td[0]) >= 2*TRACKS_PER_DAY/3
@@ -332,7 +382,7 @@ for i in range(0,len(rx1list)):
 					fig,(ax1,ax2)= plt.subplots(2,sharex=False,figsize=(8,11))
 					title = rx1.upper() + ' - ' + rx2.upper() + ' ' + c.upper() + 'NO DATA'
 					fig.suptitle(title,ha='left',x=0.02,size='medium')	
-					plt.savefig(os.path.join(tmpDir,rx1 + '.' + rx2 + '.' + c + '.ps'),papertype='a4')
+					plt.savefig(os.path.join(tmpDir,rx1 + '.' + rx2 + '.' + c + '.png'))
 					continue
 					
 				# Now we fix up for known events 
@@ -357,7 +407,8 @@ for i in range(0,len(rx1list)):
 				
 				(tdtaus, tddevs, tderrors, tdns) = allantools.tdev(td[1],rate = 1.0,taus='all')
 				
-				fig,(ax1,ax2)= plt.subplots(2,sharex=False,figsize=(8,11))
+				fig,(ax1,ax2)= plt.subplots(2,sharex=False,figsize=(8,9))
+				plt.subplots_adjust(bottom=0.07,right=0.85)
 				title = rx1.upper() + ' - ' + rx2.upper() + ' ' + c.upper()
 				fig.suptitle(title,ha='left',x=0.02,size='medium')
 				
@@ -366,7 +417,7 @@ for i in range(0,len(rx1list)):
 				ax1.set_xlabel('MJD')
 				ax1.plot(td[0],td[1],ls='None',marker='.')
 				ax1.plot(td[0],mm)
-				ax1.set_xlim([startMJD,stopMJD]) # mask out unmwanted events
+				ax1.set_xlim([startMJD,stopMJD+1]) # mask out unmwanted events
 				PlotEventMarkers(ax1,rx1events,rx1evcol)
 				PlotEventMarkers(ax1,rx2events,rx2evcol)
 				
@@ -379,9 +430,96 @@ for i in range(0,len(rx1list)):
 
 				ax2.grid(which='both')
 				
-				plt.savefig(os.path.join(tmpDir,rx1 + '.' + rx2 + '.' + c + '.ps'),papertype='a4')
+				plotFile = os.path.join(tmpDir,rx1 + '.' + rx2 + '.' + c + '.png')
+				tmpFiles.append(plotFile)
+				plotFiles.append([plotFile,rx1.upper(),rx2.upper(),c.upper()]) # may as well save stuff
+				plt.savefig(plotFile)
 				
 
 if (args.display):
 	plt.show()
-				
+
+if (args.report):
+	
+	dt = datetime.today()
+	yyyy = dt.year
+	mm  = dt.month
+	dd = dt.day
+
+	html =  '<html>'
+	html += '<head></head>'
+	html += '<body>'
+	html += '<H2>' + 'GNSS receiver common clock comparison report for MJD {:d} to {:d}'.format(startMJD,stopMJD) + '</H2>'
+	html += '<br>'
+	html += 'Notes:<br>'
+	html += '<ol>'
+	html += '<li>Running average (in orange) is over 12 hours.</li>'
+	html += '<li>Known  steps have been fixed using event data.</li>'
+	html += '<li>Events are marked with green lines for the first receiver, and with purple lines for the second receiver.</li>'
+	html += '<li>Excluded data are marked as shaded rectangles, in lighter shades of green and purple.</li>'
+	html += '</ol>'
+	html += '<br> <em>Generated by ' + appName + ' ' + VERSION + ' (' + user + '@' + socket.gethostname() + ' </em>)</br>\n' 
+	
+	# Add the plots
+	for pf in plotFiles:
+		plotCID = '{}{}{}'.format(pf[1],pf[2],pf[3])
+		plotAltText = 'Receivers {}-{} signal {}'.format(pf[1],pf[2],pf[3])
+		html += '<img src="cid:' + plotCID  +'" alt="' + plotAltText + '">'
+	
+	html += '</body>'
+	html += '</html>'
+	
+	repBody = html
+	# Hacky fixup - replace CIDs with references to image files for generating the report
+	for pf in plotFiles:
+		plotCID = '{}{}{}'.format(pf[1],pf[2],pf[3])
+		repBody = repBody.replace('cid:'+plotCID,pf[0])
+	repName = 'rep.{:d}{:02d}{:02d}'.format(yyyy,mm,dd)
+	repHTML = os.path.join(tmpDir,repName + '.html')
+	repPDF  = os.path.join(tmpDir,repName + '.pdf')
+	fout = open(repHTML,'w')
+	fout.write(repBody)
+	fout.close()
+	
+	# Create a pdf from html
+	cmd = '/usr/bin/htmldoc --quiet --webpage -f ' + repPDF + ' ' + repHTML
+	os.system(cmd)
+
+if args.email and args.report:
+	
+
+	msg = MIMEMultipart('related')
+	msg['Subject'] = 'GNSS receiver time-transfer performance for {} (MJD {:d} to {:d})'.format(repInterval,startMJD,stopMJD) 
+	msg['From'] = emailSender
+	msg['To'] = emailRecipients
+	msg['Reply-To'] = emailSender
+
+	body = MIMEText(html,'html')
+	msg.attach(body)
+
+	for pf in plotFiles:
+		fp = open(pf[0], 'rb')
+		msgImage = MIMEImage(fp.read())
+		fp.close()
+		plotCID = '{}{}{}'.format(pf[1],pf[2],pf[3])
+		msgImage.add_header('Content-ID', '<'+plotCID+'>')
+		msg.attach(msgImage)
+
+	# Send the message via local SMTP server.
+	try:
+		s = smtplib.SMTP(smtpServer)
+	except Exception as e:
+		print('Exception:',e)
+		ottp.ErrorExit('Unable to connect to ' + smtpServer)
+			
+	s.sendmail([emailSender],[emailRecipients], msg.as_string())
+	s.quit()
+
+	
+if not(debug):
+	tmpFiles.append(os.path.join(tmpDir,'ref.cal.matches.txt'))
+	tmpFiles.append(os.path.join(tmpDir,'ref.cal.ps'))
+	for f in tmpFiles:
+		os.unlink(f)
+	if args.report:
+		os.unlink(repHTMl)
