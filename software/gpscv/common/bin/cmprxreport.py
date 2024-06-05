c45b150fe63bf655c9d1b0016d34a2dd321d2ad4
groundstate <michaeljwouters@gmail.com>
2024-06-05 05:39:41 +0000

    Itty bitty buggy wuggy. Be more selective about files to be cleaned up.

--- software/gpscv/common/bin/cmprxreport.py
+++ software/gpscv/common/bin/cmprxreport.py
@@ -55,7 +55,7 @@ try:
 except ImportError:
 	sys.exit('ERROR: Must install ottplib\n eg openttp/software/system/installsys.py -i ottplib')
 
-VERSION = "0.3.0"
+VERSION = "0.3.1"
 AUTHORS = "Michael Wouters"
 
 MODE_CMPALL = 0
@@ -665,8 +665,9 @@ if args.email and args.report:
 
 	
 if not(debug):
-	tmpFiles.append(os.path.join(tmpDir,'ref.cal.matches.txt'))
-	tmpFiles.append(os.path.join(tmpDir,'ref.cal.ps'))
+	if dataSource == SRC_CGGTTS:
+		tmpFiles.append(os.path.join(tmpDir,'ref.cal.matches.txt'))
+		tmpFiles.append(os.path.join(tmpDir,'ref.cal.ps'))
 	for f in tmpFiles:
 		os.unlink(f)
 	if args.report:
